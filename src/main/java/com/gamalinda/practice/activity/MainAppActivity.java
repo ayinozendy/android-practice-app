501ddfb3970c93e0486ee7f6cc061dcdf351bdaf
Ino Gamalinda <ino.gamalinda@teamcodeflux.com>
2013-08-07 22:34:13 +0800

    Rename package typo paractice to practice
    Signed-off-by: Ino Gamalinda <ino.gamalinda@teamcodeflux.com>

--- src/main/java/com/gamalinda/practice/activity/MainAppActivity.java
+++ src/main/java/com/gamalinda/practice/activity/MainAppActivity.java
@@ -1,8 +1,8 @@
-package com.gamalinda.paractice.activity;
+package com.gamalinda.practice.activity;
 
 import android.app.Activity;
 import android.os.Bundle;
-import com.gamalinda.paractice.R;
+import com.gamalinda.practice.R;
 import com.googlecode.androidannotations.annotations.EActivity;
 
 @EActivity(R.layout.main)
