6a575d706323648c9442b87095cea7a2e2fe57d3
Ino Gamalinda <ino.gamalinda@teamcodeflux.com>
2013-08-07 23:17:22 +0800

    Add navigation drawer with simple sample items
    Signed-off-by: Ino Gamalinda <ino.gamalinda@teamcodeflux.com>

--- src/main/java/com/gamalinda/practice/activity/MainAppActivity.java
+++ src/main/java/com/gamalinda/practice/activity/MainAppActivity.java
@@ -1,15 +1,35 @@
 package com.gamalinda.practice.activity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import com.actionbarsherlock.app.SherlockActivity;
 import com.gamalinda.practice.R;
+import com.googlecode.androidannotations.annotations.AfterViews;
 import com.googlecode.androidannotations.annotations.EActivity;
+import com.googlecode.androidannotations.annotations.ViewById;
+import com.googlecode.androidannotations.annotations.res.StringArrayRes;
+
+import java.util.ArrayList;
 
 @EActivity(R.layout.main)
 public class MainAppActivity extends SherlockActivity {
+
+    @StringArrayRes
+    String[] things;
+
+    @ViewById(R.id.left_drawer)
+    android.widget.ListView leftDrawer;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().show();
     }
+
+    @AfterViews
+    void afterViews() {
+        getSupportActionBar().show();
+        leftDrawer.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, things));
+    }
+
 }
