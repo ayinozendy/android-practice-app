24087d6182d4a09e395fe01832d15bf15c8b89a3
Ino Gamalinda <ino.gamalinda@teamcodeflux.com>
2013-08-07 22:44:17 +0800

    Convert MainAppActivity to SherlockActivity
    Also add Theme.Sherlock.Light as Theme at AndroidManifest
    required by ActionBarSherlock.
    
    Signed-off-by: Ino Gamalinda <ino.gamalinda@teamcodeflux.com>

--- src/main/java/com/gamalinda/practice/activity/MainAppActivity.java
+++ src/main/java/com/gamalinda/practice/activity/MainAppActivity.java
@@ -1,15 +1,15 @@
 package com.gamalinda.practice.activity;
 
-import android.app.Activity;
 import android.os.Bundle;
+import com.actionbarsherlock.app.SherlockActivity;
 import com.gamalinda.practice.R;
 import com.googlecode.androidannotations.annotations.EActivity;
 
 @EActivity(R.layout.main)
-public class MainAppActivity extends Activity {
+public class MainAppActivity extends SherlockActivity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
+        getSupportActionBar().show();
     }
 }
