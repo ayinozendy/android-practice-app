12788576c0a3e79b8003d0a91639873d2d6f86c5
Ino Gamalinda <ino.gamalinda@teamcodeflux.com>
2013-08-07 22:13:07 +0800

    Move MainAppActivity to activity package level
    Signed-off-by: Ino Gamalinda <ino.gamalinda@teamcodeflux.com>

---
+++ src/main/java/com/gamalinda/paractice/activity/MainAppActivity.java
@@ -0,0 +1,13 @@
+package com.gamalinda.paractice.activity;
+
+import android.app.Activity;
+import android.os.Bundle;
+import com.gamalinda.paractice.R;
+
+public class MainAppActivity extends Activity {
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
