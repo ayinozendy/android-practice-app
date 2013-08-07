8211f56bc5bcb0ab47adb429b1930938b849c19c
Ino Gamalinda <ino.gamalinda@teamcodeflux.com>
2013-08-07 21:48:30 +0800

    Initial commit
    Signed-off-by: Ino Gamalinda <ino.gamalinda@teamcodeflux.com>

---
+++ src/main/java/com/gamalinda/paractice/MainAppActivity.java
@@ -0,0 +1,15 @@
+package com.gamalinda.paractice;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainAppActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
