1dd8ab0cef79d7cdaf839c825be16463d3e8c13f
Ino Gamalinda <ino.gamalinda@teamcodeflux.com>
2013-08-07 22:27:25 +0800

    Convert MainAppActivity to Android Annotations Activity
    Signed-off-by: Ino Gamalinda <ino.gamalinda@teamcodeflux.com>

--- src/main/java/com/gamalinda/paractice/activity/MainAppActivity.java
+++ src/main/java/com/gamalinda/paractice/activity/MainAppActivity.java
@@ -3,11 +3,13 @@ package com.gamalinda.paractice.activity;
 import android.app.Activity;
 import android.os.Bundle;
 import com.gamalinda.paractice.R;
+import com.googlecode.androidannotations.annotations.EActivity;
 
+@EActivity(R.layout.main)
 public class MainAppActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+
     }
 }
