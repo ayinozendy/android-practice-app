784528247e7ef806afbe87fba566b543d1761a76
Ino Gamalinda <ino.gamalinda@teamcodeflux.com>
2013-08-08 20:35:41 +0800

    Create Upload View
    Signed-off-by: Ino Gamalinda <ino.gamalinda@teamcodeflux.com>

---
+++ src/main/java/com/gamalinda/practice/activity/UploadViewActivity.java
@@ -0,0 +1,10 @@
+package com.gamalinda.practice.activity;
+
+import com.actionbarsherlock.app.SherlockActivity;
+import com.gamalinda.practice.R;
+import com.googlecode.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.upload_view)
+public class UploadViewActivity extends SherlockActivity {
+
+}
