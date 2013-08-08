905072e2785864e1be509630ac68868e8786de6f
Ino Gamalinda <ino.gamalinda@teamcodeflux.com>
2013-08-08 17:52:52 +0800

    Create sahre view
    Signed-off-by: Ino Gamalinda <ino.gamalinda@teamcodeflux.com>

---
+++ src/main/java/com/gamalinda/practice/activity/ShareViewActivity.java
@@ -0,0 +1,10 @@
+package com.gamalinda.practice.activity;
+
+import com.actionbarsherlock.app.SherlockFragment;
+import com.gamalinda.practice.R;
+import com.googlecode.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.share_view)
+public class ShareViewActivity extends SherlockFragment {
+
+}
