71acf87755416dc662c060eb51b6636fd5c45bd3
Ino Gamalinda <ino.gamalinda@teamcodeflux.com>
2013-08-09 00:21:46 +0800

    Use getContentResolver instead of managedQuery
    managedQuery is deprecated.
    
    Signed-off-by: Ino Gamalinda <ino.gamalinda@teamcodeflux.com>

--- src/main/java/com/gamalinda/practice/activity/UploadViewActivity.java
+++ src/main/java/com/gamalinda/practice/activity/UploadViewActivity.java
@@ -80,7 +80,7 @@ public class UploadViewActivity extends SherlockActivity {
 
     public String getRealPathFromURI(Uri contentUri) {
         String[] proj = {MediaStore.Images.Media.DATA};
-        Cursor cursor = managedQuery(contentUri, proj, null, null, null);
+        Cursor cursor = getContentResolver().query(contentUri, proj, null, null, null);
         int column_index = cursor.getColumnIndexOrThrow(MediaStore.Images.Media.DATA);
         cursor.moveToFirst();
         return cursor.getString(column_index);
