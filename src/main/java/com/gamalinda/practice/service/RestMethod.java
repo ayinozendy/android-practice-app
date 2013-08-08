4141ee49a4a9c80f52b48a2f886da1d529693c77
Ino Gamalinda <ino.gamalinda@teamcodeflux.com>
2013-08-09 00:18:51 +0800

    Add request headers
    Signed-off-by: Ino Gamalinda <ino.gamalinda@teamcodeflux.com>

--- src/main/java/com/gamalinda/practice/service/RestMethod.java
+++ src/main/java/com/gamalinda/practice/service/RestMethod.java
@@ -1,11 +1,18 @@
 package com.gamalinda.practice.service;
 
+import android.util.Log;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.ByteArrayHttpMessageConverter;
 import org.springframework.http.converter.FormHttpMessageConverter;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.http.converter.json.MappingJacksonHttpMessageConverter;
+import org.springframework.util.MultiValueMap;
 import org.springframework.web.client.RestTemplate;
 
+import java.net.URI;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -13,6 +20,18 @@ public class RestMethod {
 
     private static RestTemplate restTemplate;
     private static RestTemplate formRestTemplate;
+
+    public static void uploadImage(String url, MultiValueMap<String, Object> parts) {
+        HttpHeaders requestHeaders = new HttpHeaders();
+        requestHeaders.setContentType(MediaType.MULTIPART_FORM_DATA);
+        HttpEntity<?> requestEntity = new HttpEntity<MultiValueMap<String, Object>>(parts, requestHeaders);
+
+        try {
+            URI result = RestMethod.getRestTemplate().postForLocation(url, requestEntity);
+        } catch (Exception e) {
+            Log.e("uploadImage", "error upload", e);
+        }
+    }
 
     //Rest Template
     private static RestTemplate getRestTemplate() {
@@ -33,6 +52,17 @@ public class RestMethod {
         List<HttpMessageConverter<?>> messageConverters = new ArrayList<HttpMessageConverter<?>>();
         messageConverters.add(new MappingJacksonHttpMessageConverter());
         messageConverters.add(new StringHttpMessageConverter());
+
+        ByteArrayHttpMessageConverter ba = new ByteArrayHttpMessageConverter();
+        //ba.setSupportedMediaTypes(Collections.singletonList(MediaType.IMAGE_JPEG));
+
+        messageConverters.add(new ByteArrayHttpMessageConverter());
+
+        FormHttpMessageConverter form = new FormHttpMessageConverter();
+        //form.setSupportedMediaTypes(Collections.singletonList(MediaType.IMAGE_JPEG));
+
+        messageConverters.add(new FormHttpMessageConverter());
+
         return messageConverters;
     }
 
