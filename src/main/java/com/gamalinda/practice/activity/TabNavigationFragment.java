13aff67530636bc2cef73106531248d6e2af3b25
Ino Gamalinda <ino.gamalinda@teamcodeflux.com>
2013-08-08 15:37:47 +0800

    Create simple tab navi fragment
    Signed-off-by: Ino Gamalinda <ino.gamalinda@teamcodeflux.com>

---
+++ src/main/java/com/gamalinda/practice/activity/TabNavigationFragment.java
@@ -0,0 +1,9 @@
+package com.gamalinda.practice.activity;
+
+import com.actionbarsherlock.app.SherlockFragment;
+import com.googlecode.androidannotations.annotations.EFragment;
+
+@EFragment
+public class TabNavigationFragment extends SherlockFragment {
+
+}
