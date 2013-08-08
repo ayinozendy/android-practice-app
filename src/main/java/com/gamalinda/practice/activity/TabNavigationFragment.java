bb55720fef5a8ac470dfbef7413f3d2b809d8340
Ino Gamalinda <ino.gamalinda@teamcodeflux.com>
2013-08-08 16:12:31 +0800

    Add layout
    Signed-off-by: Ino Gamalinda <ino.gamalinda@teamcodeflux.com>

--- src/main/java/com/gamalinda/practice/activity/TabNavigationFragment.java
+++ src/main/java/com/gamalinda/practice/activity/TabNavigationFragment.java
@@ -1,9 +1,10 @@
 package com.gamalinda.practice.activity;
 
 import com.actionbarsherlock.app.SherlockFragment;
+import com.gamalinda.practice.R;
 import com.googlecode.androidannotations.annotations.EFragment;
 
-@EFragment
+@EFragment(R.layout.tab_navigation_fragment)
 public class TabNavigationFragment extends SherlockFragment {
 
 }
