33cdbe5e4ac0ca9edd48843525fd2d33ab964c71
Ino Gamalinda <ino.gamalinda@teamcodeflux.com>
2013-08-08 20:56:53 +0800

    Create rest method
    Signed-off-by: Ino Gamalinda <ino.gamalinda@teamcodeflux.com>

---
+++ src/main/java/com/gamalinda/practice/service/RestMethod.java
@@ -0,0 +1,60 @@
+package com.gamalinda.practice.service;
+
+import org.springframework.http.converter.FormHttpMessageConverter;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
+import org.springframework.http.converter.json.MappingJacksonHttpMessageConverter;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class RestMethod {
+
+    private static RestTemplate restTemplate;
+    private static RestTemplate formRestTemplate;
+
+    //Rest Template
+    private static RestTemplate getRestTemplate() {
+        if (restTemplate == null) {
+            restTemplate = createRestTemplate();
+        }
+
+        return restTemplate;
+    }
+
+    private static RestTemplate createRestTemplate() {
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.setMessageConverters(messageConverters());
+        return restTemplate;
+    }
+
+    private static List<HttpMessageConverter<?>> messageConverters() {
+        List<HttpMessageConverter<?>> messageConverters = new ArrayList<HttpMessageConverter<?>>();
+        messageConverters.add(new MappingJacksonHttpMessageConverter());
+        messageConverters.add(new StringHttpMessageConverter());
+        return messageConverters;
+    }
+
+    //Form Rest Template
+    private static RestTemplate getFormRestTemplate() {
+        if (formRestTemplate == null) {
+            formRestTemplate = createFormRestTemplate();
+        }
+
+        return formRestTemplate;
+    }
+
+    private static RestTemplate createFormRestTemplate() {
+        RestTemplate restTemplate = new RestTemplate();
+        restTemplate.setMessageConverters(formConverters());
+        return restTemplate;
+    }
+
+    private static List<HttpMessageConverter<?>> formConverters() {
+        List<HttpMessageConverter<?>> messageConverters = new ArrayList<HttpMessageConverter<?>>();
+        messageConverters.add(new FormHttpMessageConverter());
+        messageConverters.addAll(messageConverters());
+        return messageConverters;
+    }
+}
