5c48e15d2e5d5449d770de1154ede148f23b0407
Ino Gamalinda <ino.gamalinda@teamcodeflux.com>
2013-08-08 14:26:46 +0800

    Create TabNavigation view and activity
    Signed-off-by: Ino Gamalinda <ino.gamalinda@teamcodeflux.com>

---
+++ src/main/java/com/gamalinda/practice/activity/TabNavigationActivity.java
@@ -0,0 +1,10 @@
+package com.gamalinda.practice.activity;
+
+import com.actionbarsherlock.app.SherlockFragmentActivity;
+import com.gamalinda.practice.R;
+import com.googlecode.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.tab_navigation_view)
+public class TabNavigationActivity extends SherlockFragmentActivity {
+
+}
