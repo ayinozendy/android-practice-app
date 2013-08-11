c12bdf41145730ce07fe236d6d517e3657a7bd59
Ino Gamalinda <ino.gamalinda@teamcodeflux.com>
2013-08-11 15:05:24 +0800

    Create list view details page view
    Signed-off-by: Ino Gamalinda <ino.gamalinda@teamcodeflux.com>

---
+++ src/main/java/com/gamalinda/practice/activity/ListViewDetailsPageActivity.java
@@ -0,0 +1,15 @@
+package com.gamalinda.practice.activity;
+
+import android.widget.TextView;
+import com.actionbarsherlock.app.SherlockActivity;
+import com.gamalinda.practice.R;
+import com.googlecode.androidannotations.annotations.AfterViews;
+import com.googlecode.androidannotations.annotations.EActivity;
+import com.googlecode.androidannotations.annotations.ViewById;
+
+@EActivity(R.layout.list_view_detail_page)
+public class ListViewDetailsPageActivity extends SherlockActivity {
+
+    @ViewById(R.id.text)
+    TextView textView;
+}
