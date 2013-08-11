b30584027e37f115e7e153c89557710cf0a85f24
Ino Gamalinda <ino.gamalinda@teamcodeflux.com>
2013-08-11 15:03:39 +0800

    Implement viewing of list
    Signed-off-by: Ino Gamalinda <ino.gamalinda@teamcodeflux.com>

--- src/main/java/com/gamalinda/practice/activity/ListViewActivity.java
+++ src/main/java/com/gamalinda/practice/activity/ListViewActivity.java
@@ -1,10 +1,32 @@
 package com.gamalinda.practice.activity;
 
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import com.actionbarsherlock.app.SherlockActivity;
 import com.gamalinda.practice.R;
+import com.googlecode.androidannotations.annotations.AfterViews;
 import com.googlecode.androidannotations.annotations.EActivity;
+import com.googlecode.androidannotations.annotations.ViewById;
+import com.googlecode.androidannotations.annotations.res.StringArrayRes;
 
 @EActivity(R.layout.list_view)
 public class ListViewActivity extends SherlockActivity {
 
+    @StringArrayRes(R.array.list_things)
+    String[] listOfThings;
+
+    @ViewById(R.id.list_view)
+    ListView listView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().show();
+    }
+
+    @AfterViews
+    void afterViews() {
+        listView.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, listOfThings));
+    }
 }
