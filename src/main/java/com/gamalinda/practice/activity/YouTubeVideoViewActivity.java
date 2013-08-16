8bc18af8dd52099b5be2e9c4086a2a9c0c2a96f7
Ino Gamalinda <ino.gamalinda@teamcodeflux.com>
2013-08-16 22:50:38 +0800

    Create Youtube video viewer activity
    Signed-off-by: Ino Gamalinda <ino.gamalinda@teamcodeflux.com>

---
+++ src/main/java/com/gamalinda/practice/activity/YouTubeVideoViewActivity.java
@@ -0,0 +1,10 @@
+package com.gamalinda.practice.activity;
+
+import com.actionbarsherlock.app.SherlockActivity;
+import com.gamalinda.practice.R;
+import com.googlecode.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.youtbe_view)
+public class YouTubeVideoViewActivity extends SherlockActivity {
+
+}
