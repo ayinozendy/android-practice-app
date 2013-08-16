8560398eb9d1e65765f7867ed15b3a6380013f1c
Ino Gamalinda <ino.gamalinda@teamcodeflux.com>
2013-08-16 23:09:58 +0800

    Implement youtube viewing intent
    Signed-off-by: Ino Gamalinda <ino.gamalinda@teamcodeflux.com>

--- src/main/java/com/gamalinda/practice/activity/YouTubeVideoViewActivity.java
+++ src/main/java/com/gamalinda/practice/activity/YouTubeVideoViewActivity.java
@@ -1,10 +1,17 @@
 package com.gamalinda.practice.activity;
 
+import android.content.Intent;
+import android.net.Uri;
 import com.actionbarsherlock.app.SherlockActivity;
 import com.gamalinda.practice.R;
+import com.googlecode.androidannotations.annotations.Click;
 import com.googlecode.androidannotations.annotations.EActivity;
 
 @EActivity(R.layout.youtbe_view)
 public class YouTubeVideoViewActivity extends SherlockActivity {
 
+    @Click(R.id.show_video)
+    void showVideo() {
+        startActivity(new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.youtube.com/watch?v=r-qbDvNBg9w")));
+    }
 }
