73cf9121b292d40a6c8af81aefdf0a88c2224bfc
Ino Gamalinda <ino.gamalinda@teamcodeflux.com>
2013-08-16 17:45:15 +0800

    Implement camera taking
    Signed-off-by: Ino Gamalinda <ino.gamalinda@teamcodeflux.com>

--- src/main/java/com/gamalinda/practice/activity/ApiPostingTestActivity.java
+++ src/main/java/com/gamalinda/practice/activity/ApiPostingTestActivity.java
@@ -1,15 +1,23 @@
 package com.gamalinda.practice.activity;
 
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.net.Uri;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
 import com.actionbarsherlock.app.SherlockActivity;
 import com.gamalinda.practice.R;
 import com.googlecode.androidannotations.annotations.Click;
 import com.googlecode.androidannotations.annotations.EActivity;
 
+import java.io.File;
+
 @EActivity(R.layout.api_posting_test_view)
 public class ApiPostingTestActivity extends SherlockActivity {
     private static final int SELECT_PHOTO = 999;
+    private static final int TAKE_PICTURE = 1000;
 
     private Uri imageUri;
 
@@ -22,6 +30,21 @@ public class ApiPostingTestActivity extends SherlockActivity {
                 if (resultCode == RESULT_OK) {
                     imageUri = imageReturnedIntent.getData();
                 }
+                break;
+            case TAKE_PICTURE:
+                if (resultCode == RESULT_OK) {
+                    Uri selectedImage = imageUri;
+                    getContentResolver().notifyChange(selectedImage, null);
+                    ContentResolver cr = getContentResolver();
+                    Bitmap bitmap;
+                    try {
+                        bitmap = android.provider.MediaStore.Images.Media
+                                .getBitmap(cr, selectedImage);
+                    } catch (Exception e) {
+                        Log.e("Camera", e.toString());
+                    }
+                }
+                break;
         }
     }
 
@@ -30,5 +53,15 @@ public class ApiPostingTestActivity extends SherlockActivity {
         Intent photoPickerIntent = new Intent(Intent.ACTION_PICK);
         photoPickerIntent.setType("image/*");
         startActivityForResult(photoPickerIntent, SELECT_PHOTO);
+    }
+
+    @Click(R.id.take_picture)
+    void takePicture() {
+        Intent intent = new Intent("android.media.action.IMAGE_CAPTURE");
+        File photo = new File(Environment.getExternalStorageDirectory(), "pic_to_post.jpg");
+        intent.putExtra(MediaStore.EXTRA_OUTPUT,
+                Uri.fromFile(photo));
+        imageUri = Uri.fromFile(photo);
+        startActivityForResult(intent, TAKE_PICTURE);
     }
 }
