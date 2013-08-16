987d8123e9a0acf0092846cf70a51a82223bd15d
Ino Gamalinda <ino.gamalinda@teamcodeflux.com>
2013-08-16 17:26:21 +0800

    Create activity API Posting
    Signed-off-by: Ino Gamalinda <ino.gamalinda@teamcodeflux.com>

---
+++ src/main/java/com/gamalinda/practice/activity/ApiPostingTestActivity.java
@@ -0,0 +1,10 @@
+package com.gamalinda.practice.activity;
+
+import com.actionbarsherlock.app.SherlockActivity;
+import com.gamalinda.practice.R;
+import com.googlecode.androidannotations.annotations.EActivity;
+
+@EActivity(R.layout.api_posting_test_view)
+public class ApiPostingTestActivity extends SherlockActivity {
+
+}
